0a8667cb42b8f2874e75befa5f2393a2fd95a8fd
Stephen Hill <stephen@gatekiller.co.uk>
2013-08-22 09:35:44 +0100

    No comment.

--- datetime.php
+++ datetime.php
@@ -4,10 +4,18 @@ date_default_timezone_set('UTC');
 
 var_dump
 (
+	"+10 Years",
 	(new DateTime())->setTimestamp(time() + 315569000)
 );
 
 var_dump
 (
+	"-10 Years",
         (new DateTime())->setTimestamp(time() - 315569000)
+);
+
+var_dump
+(
+	"UTC",
+	new DateTime()
 );
